3e92fa0abf9285b53a2c4b091f1f2a3f540e73ed
philip-webdev <philisobank@gmail.com>
2024-10-16 16:01:18 +0100

    twa backend

--- server.js
+++ server.js
@@ -20,9 +20,7 @@ app.get('/telegram-webApp/orderly', async (req, res) => {
     const response = await axios.get('https://twa-backend-g83o.onrender.com/telegram-webApp/orderly');
     console.log(response.data); }
 );
-    //const rawData = [product_data_1, product_data_2, product_data_3, product_data_4,product_data_5,product_data_6,product_data_7, product_data_8, product_data_9 ];
-  // const rawData = product_data_1;
-     //  res.json(rawData);});
+    
 
 
 app.listen(PORT, () => {
