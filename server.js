6533bf1fb4f69690d543465ceb6c226a20b25ef3
philip-webdev <philisobank@gmail.com>
2024-10-18 11:18:04 +0100

    twa backend

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const dbURI = 'mongodb+srv://phil_web:test123@cluster0.wsfgf9o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 mongoose.connect(dbURI);
+app.use(express.urlencoded({extended:true}));
 // Configure CORS to allow requests from your frontend domain
 const corsOptions = {
     origin: 'https://philip-webdev.github.io', // Replace with your GitHub Pages URL
@@ -19,17 +20,17 @@ app.use(cors(corsOptions));
 
 // Example API endpoint
 app.get('/telegram-webApp/orderly',  (req, res) => {
-    const response =  axios.get('https://twa-backend-g83o.onrender.com/telegram-webApp/orderly');
-    console.log(response.data); }
+   // const response =  axios.get('https://twa-backend-g83o.onrender.com/telegram-webApp/orderly');
+    console.log(res.json(result)); }
 );
     
-app.post('https://twa-backend-g83o.onrender.com/profile', async(req, res)=>{
+app.post('#/profiler', (req, res)=>{
 const freshPerson = new Profile(req.body);
 
 freshPerson.save()
 .then((result)=>{res.json(result)})
 
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
