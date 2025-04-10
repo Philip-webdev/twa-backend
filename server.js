65375b0138dc1b54106e8df129cd662a1c3c17d5
philip-webdev <philisobank@gmail.com>
2025-04-10 11:09:38 +0100

    new

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ try{
 
     app.get('/wallets',  async (req, res) => {
         try {
-            const {   addresses: Address  } = req.body;
+            const {   addresses: Address  } = req.query;
             const resultAddresses = await Account.findOne({addresses});  
            
             res.json(resultAddresses);
