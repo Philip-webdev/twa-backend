4882143d8e435e12e52f798c48f639acaf3f3cd7
philip-webdev <philisobank@gmail.com>
2025-05-13 16:13:26 +0100

    new

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(dbURI)
         next();
     });
 const corsOptions = {
-    origin: ['https://nexr-pi.vercel.app','https://philip-webdev.github.io',  'https://sandbox.monnify.com', 'http://localhost:5173'  ],
+    origin: ['https://nexr-pi.vercel.app','https://philip-webdev.github.io',  'https://sandbox.monnify.com', 'http://localhost:5173' , 'https://nekstpei.com','https://app.nekstpei.com' ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
